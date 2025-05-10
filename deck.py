2cb0b269cf76e6359301b3c4e083dce3772e41a1
VM_N <necvladm@gmail.com>
2025-05-10 12:56:20 +0300

    add deck

---
+++ deck.py
@@ -0,0 +1,42 @@
+import random
+
+NUMBERS = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+KIND = ["S", "C", "H", "D"]
+
+class Deck:
+    def __init__(self):
+        self.deck = []
+        self.seen = set()
+        self.hand = []
+        for k in KIND:
+            for n in NUMBERS:
+                card = f"{n}" + k
+                self.deck.append(card)
+
+    def draw_card(self):
+        new_card = random.randint(0, 51)
+        return self.deck[new_card]
+
+    def first_draw(self):
+        while len(self.hand) < 5:
+            new_card = self.draw_card()
+            if new_card not in self.seen:
+                self.seen.add(new_card)
+                self.hand.append(new_card)
+    
+    def second_draw(self, change=list()):
+        if len(change) > 0:
+            change.sort()
+            new_hand = []
+            for n in change:
+                if n > 4 or n < 0:
+                    print(f"{n} not a valid card number")
+                    self.card_drop()
+            for i in range(0, 5):
+                if i not in change:
+                    new_hand.append(self.hand[i])
+
+
+    def card_drop(self):
+        drop = input("Choose cards to drop (0-4), with space between each number").split(" ")
+        self.second_draw(change=drop)
---
+++ main.py
@@ -0,0 +1,5 @@
+from deck import *
+
+new_deck = Deck()
+
+print(new_deck.deck)
